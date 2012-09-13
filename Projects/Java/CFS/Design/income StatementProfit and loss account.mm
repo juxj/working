3a0d0ead6c2385f1dede2c99139cbff6c26910e2
juxj <juxj@hotmail.com>
2012-09-13 17:04:56 +0800

    dd

---
+++ Projects/Java/CFS/Design/income StatementProfit and loss account.mm
@@ -0,0 +1,16 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1347264263288" ID="Freemind_Link_1560739411" MODIFIED="1347264573254" STYLE="bubble" TEXT="Profit &amp;&#xa;Profit Appropriation Statement">
+<node CREATED="1347264394945" ID="_" MODIFIED="1347264399711" POSITION="right" TEXT="&#x57fa;&#x7840;&#x9879;&#x76ee;">
+<node CREATED="1347264421664" ID="Freemind_Link_1758534997" MODIFIED="1347264446362" TEXT="1. &#x7ecf;&#x8425;&#x6d3b;&#x52a8;&#x4ea7;&#x751f;&#x7684;&#x73b0;&#x91d1;&#x6d41;&#x91cf;"/>
+<node CREATED="1347264446882" ID="Freemind_Link_1253845139" MODIFIED="1347264462076" TEXT="2. &#x6295;&#x8d44;&#x6d3b;&#x52a8;&#x4ea7;&#x751f;&#x7684;&#x73b0;&#x91d1;&#x6d41;&#x91cf;"/>
+<node CREATED="1347264462595" ID="Freemind_Link_150004366" MODIFIED="1347264486355" TEXT="3. &#x7b79;&#x8d44;&#x6d3b;&#x52a8;&#x4ea7;&#x751f;&#x7684;&#x73b0;&#x91d1;&#x6d41;&#x91cf;"/>
+<node CREATED="1347264502091" ID="Freemind_Link_1558259779" MODIFIED="1347264504322" TEXT="4. &#x6c47;&#x7387;&#x53d8;&#x52a8;&#x5bf9;&#x73b0;&#x91d1;&#x7684;&#x5f71;&#x54cd;"/>
+</node>
+<node CREATED="1347264403166" ID="Freemind_Link_1752928407" MODIFIED="1347264414330" POSITION="left" TEXT="&#x8865;&#x5145;&#x8d44;&#x6599;">
+<node CREATED="1347264523712" ID="Freemind_Link_1672310969" MODIFIED="1347264524522" TEXT="1&#x3001;&#x5c06;&#x51c0;&#x5229;&#x6da6;&#x8c03;&#x8282;&#x4e3a;&#x7ecf;&#x8425;&#x6d3b;&#x52a8;&#x73b0;&#x91d1;&#x6d41;&#x91cf;"/>
+<node CREATED="1347264535411" ID="Freemind_Link_446131789" MODIFIED="1347264536250" TEXT="2&#x3001;&#x4e0d;&#x6d89;&#x53ca;&#x73b0;&#x91d1;&#x6536;&#x652f;&#x7684;&#x6295;&#x8d44;&#x548c;&#x7b79;&#x8d44;&#x6d3b;&#x52a8;&#xff1a;"/>
+<node CREATED="1347264543812" ID="Freemind_Link_1898818512" MODIFIED="1347264544528" TEXT="3&#x3001;&#x73b0;&#x91d1;&#x53ca;&#x73b0;&#x91d1;&#x7b49;&#x4ef7;&#x7269;&#x51c0;&#x589e;&#x52a0;&#x60c5;&#x51b5;&#xff1a;"/>
+</node>
+</node>
+</map>
